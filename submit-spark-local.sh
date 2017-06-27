84f536a147d8d0262ca20fb5f178b15e2e9401be
wangke <wangke3@xiaomi.com>
2017-06-27 15:17:34 +0800

    fix compile rule
    Signed-off-by: wangke <wangke3@xiaomi.com>

--- submit-spark-local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-$SPARK_HOME/bin/spark-submit --class spark.test.WordCount --master local[8]  target/wordCount-jar-with-dependency.jar file:///home/wangke/workspace/logs/bednets/phish_1.3  file:///home/wangke/workspace/logs/loglog/output
